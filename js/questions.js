494836dfe568027691fb1227f7ec241a311ea2da
Ixetan <foker11163@gmail.com>
2025-05-25 15:17:44 +0800

    Осталось добавить поинты

--- js/questions.js
+++ js/questions.js
@@ -34,18 +34,45 @@ const questions = [
     },
 ]
 
-var currentQuestion = 0
+var currentQuestion = 0;
+var answerGiven = false;
+var valor = 0;
+
+const nextButton = document.getElementById('next_button');
+const skipButton = document.getElementById('skip_button');
 
 function selectAnswer(event, correct_answer){
+    if (answerGiven)
+        return
+
+    answerGiven = true
     const target = event.target;
+
+    nextButton.classList.remove('hidden')
+    skipButton.classList.add('hidden')
     
-    if (correct_answer == target.getAttribute('data-answer-index'))
+    if (correct_answer == target.getAttribute('data-answer-index')){
         target.classList.add('correct')
-    else
+        target.classList.remove('neutral')
+        valor += 1
+    }
+    else {
         target.classList.add('wrong')
+        target.classList.remove('neutral')
+        const correctAnswerDiv = document.getElementById('correct_answer')
+        correctAnswerDiv.classList.add('correct')
+        correctAnswerDiv.classList.remove('neutral')
+    }
+
+
 }
 
 function  newQuestion(){
+    answerGiven = false
+
+    nextButton.classList.add('hidden')
+    skipButton.classList.remove('hidden')
+
     const questionsContainer = document.getElementById("question_container")
     const answersContainer = document.getElementById("answers_container")
 
@@ -62,14 +89,22 @@ function  newQuestion(){
         const answer = question.answers[i]
         const newAnswer = document.createElement('div')
         newAnswer.classList.add('answer')
+        newAnswer.classList.add('neutral')
         newAnswer.setAttribute('data-answer-index', i)
         newAnswer.innerText = answer
         newAnswer.setAttribute('onclick', `selectAnswer(event, ${question.correct_answer})`)
+        if (i == question.correct_answer)
+            newAnswer.id = 'correct_answer'
         answersContainer.append(newAnswer)
     }
 }   
 
 function skip(){
+    currentQuestion += 1
+    newQuestion()
+}
+
+function next(){
     currentQuestion += 1
     newQuestion()
 }
